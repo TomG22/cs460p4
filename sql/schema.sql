c7012f4c47041b9ed0f6ea47f13ccebbb19663a7
gabeHernandez-I <gabehernandez07@arizona.edu>
2026-05-03 13:01:39 -0700

    Making debug changes and changing some data types to match JDBC code

--- sql/schema.sql
+++ sql/schema.sql
@@ -5,11 +5,10 @@
 CREATE TABLE ApplicationUser {
 	userID            INTEGER PRIMARY KEY,
 	userName          VARCHAR2(50),
-	userEmail         VARCHAR2(50),
-	creationDate      VARCHAR2(9), -- MM/DD/YYYY
+	email             VARCHAR2(50),
+	creationDate      DATE,
 	preferredLanguage VARCHAR2(20),
 	tierID            VARCHAR2(50),
-    PRIMARY KEY       userID
 };
 
 /*-------------------------------*
@@ -18,8 +17,8 @@ CREATE TABLE ApplicationUser {
 CREATE TABLE MembershipTier {
     tierID            INTEGER PRIMARY KEY,
     tierName          VARCHAR2(50),
-    maxMessagesPerDay Number(6), -- Guessing 999,999 max messages is enough
-    proModelStatus    Number(1), -- True or false (1 or 0)
+    maxMessagesPerDay Number(6),   -- Guessing 999,999 max messages is enough
+    proModelStatus    Number(1),   -- True or false (1 or 0)
     monthlyFee        Number(10,2) -- Accounting for cents
 };
 
@@ -38,10 +37,10 @@ CREATE TABLE BillingProfile {
  *------------------------*/
 CREATE TABLE Invoice {
     invoiceID     INTEGER PRIMARY KEY,
-    userID        INTEGER, -- FK to AppUser
-    amount        INTEGER,
-    invoiceDate   VARCHAR(9), -- MM/DD/YYYY
-    paymentStatus NUMBER(1) -- True or false (1 or 0)
+    userID        INTEGER,      -- FK to AppUser
+    amount        NUMBER(10,2), -- Accounting for cents
+    invoiceDate   DATE,
+    paymentStatus NUMBER(1)     -- True or false (1 or 0)
 };
 
 /*------------------------------*
@@ -49,10 +48,10 @@ CREATE TABLE Invoice {
  *------------------------------*/
 CREATE TABLE SupportTicket {
     ticketID       INTEGER PRIMARY KEY,
-    userID         INTEGER,     -- FK to AppUser
-    agentID        INTEGER,     -- FK to SuppAgent
+    userID         INTEGER, -- FK to AppUser
+    agentID        INTEGER, -- FK to SuppAgent
     topic          VARCHAR2(1000),
-    dateOpened     VARCHAR2(9), -- MM/DD/YYYY
+    dateOpened     DATE,
     resolutionDays NUMBER(5),
     outcome        VARCHAR2(1000)
 };
@@ -61,8 +60,8 @@ CREATE TABLE SupportTicket {
  | SupportAgent Table Creation |
  *-----------------------------*/
 CREATE TABLE SupportAgent {
-    agentID INTEGER PRIMARY KEY,
-    agentName VARCHAR2(50),
+    agentID    INTEGER PRIMARY KEY,
+    agentName  VARCHAR2(50),
     agentEmail VARCHAR2(50)
 };
 
@@ -71,10 +70,10 @@ CREATE TABLE SupportAgent {
  *--------------------------*/
 CREATE TABLE Workspace {
     workspaceID   INTEGER PRIMARY KEY,
+    creatorID     INTEGER, -- FK to AppUser
     workSpaceName VARCHAR2(50),
     privateStatus Number(1),
-    creatorID     INTEGER, -- FK to AppUser
-    creationDate  VARCHAR2(9)
+    creationDate  DATE
 };
 
 /*------------------------------------*
@@ -83,7 +82,7 @@ CREATE TABLE Workspace {
 CREATE TABLE WorkspaceMembership {
     userID      INTEGER,
     workspaceID INTEGER, -- FK to Workspace
-    joinDate    VARCHAR2(9)
+    joinDate    DATE
 };
 
 /*-----------------------------*
@@ -95,7 +94,7 @@ CREATE TABLE Conversation {
     workspaceID    INTEGER, -- FK to Workspace
     personaID      INTEGER, -- FK to Persona
     title          VARCHAR2(50),
-    creationDate   VARCHAR2(9),
+    creationDate   DATE,
     activeStatus   NUMBER(1)
 };
 
@@ -107,18 +106,18 @@ CREATE TABLE Message {
     conversationID INTEGER,     -- FK to Conversation
     messageRole    VARCHAR2(20),
     content        VARCHAR2(1000),
-    timeSent       VARCHAR2(11) -- XX:XX:XX:AM or XX:XX:XX:PM
+    timeSent       TIMESTAMP
 };
 
 /*-------------------------*
  | Feedback Table Creation |
  *-------------------------*/
 CREATE TABLE Feedback {
-    feedbackID INTEGER PRIMARY KEY,
-    messageID INTEGER,          -- FK to Message
-    rating Number(3),           -- Rating between 0 and 100
-    feedbackText VARCHAR2(1000),
-    timeSubmitted VARCHAR2(11), -- XX:XX:XX:AM or XX:XX:XX:PM
+    feedbackID    INTEGER PRIMARY KEY,
+    messageID     INTEGER,   -- FK to Message
+    rating        Number(3), -- Rating between 0 and 100
+    feedbackText  VARCHAR2(1000),
+    timeSubmitted TIMESTAMP
 };
 
 /*-------------------------*
@@ -127,18 +126,18 @@ CREATE TABLE Feedback {
 CREATE TABLE Bookmark {
     userID         INTEGER PRIMARY KEY,
     messageID      INTEGER,      -- FK to Message
-    timeBookmarked VARCHAR2(11), -- XX:XX:XX:AM or XX:XX:XX:PM
+    timeBookmarked TIMESTAMP
 };
 
 /*------------------------*
  | Persona Table Creation |
  *------------------------*/
 CREATE TABLE Persona {
-    personalID         INTEGER PRIMARY KEY,
-    personaName VARCHAR2(50),
-    instructions VARCHCAR2(500)
-    creatorID INTEGGER, -- FK to UserApp
-    creationDat VARCHAR2(11), -- XX:XX:XX:AM or XX:XX:XX:PM
+    personalID   INTEGER PRIMARY KEY,
+    personaName  VARCHAR2(50),
+    instructions VARCHCAR2(500),
+    creatorID    INTEGER, -- FK to UserApp
+    creationDate DATE
 };
 
 /*-------------------------------*
@@ -146,11 +145,11 @@ CREATE TABLE Persona {
  *-------------------------------*/
 CREATE TABLE PromptTemplate {
     templateID    INTEGER PRIMARY KEY,
-    creatorID     INTEGER,        -- FK to AppUser
-    workspaceID   INTEGER,        -- FK to Workspace
-    title         VARCHAR2(50),   -- FK to Message
-    content       VARCHAR2(1000), -- XX:XX:XX:AM or XX:XX:XX:PM
+    creatorID     INTEGER,      -- FK to AppUser
+    workspaceID   INTEGER,      -- FK to Workspace
+    title         VARCHAR2(50), -- FK to Message
+    content       VARCHAR2(1000),
     category      VARCHAR2(50),
     privateStatus NUMBER(1),
-    creationDate  VARCHAR2(9) MM/DD/YYYY
+    creationDate  DATE
 };
