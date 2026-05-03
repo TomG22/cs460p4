19675626a93e9f17f85c49ac97806a79556f9b9c
gabeHernandez-I <gabehernandez07@arizona.edu>
2026-05-03 13:28:18 -0700

    HUGE error found and changed (Brackets to parenthesis)

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,52 +1,62 @@
+/*---------------------------------------------------------------*
+ | Author: Gabriel Hernandez                                     |
+ | File Name: schema.sql                                         |
+ | Creation Date: 05-02-2026                                     |
+ | Description: SQL queries to create the tables associated with |
+ |     our relations. Has correct PK-FK relationships and the    |
+ |     appropriate constraints.                                  |
+ | Last Modification: 05-03-2026                                 |
+ *---------------------------------------------------------------*/
+
 /*--------------------------------*
  | ApplicationUser Table Creation |
  *--------------------------------*/
  -- Ranges for varchar2, number, and using integer can be changed
-CREATE TABLE ApplicationUser {
+CREATE TABLE ApplicationUser (
 	userID            INTEGER PRIMARY KEY,
 	userName          VARCHAR2(50),
 	email             VARCHAR2(50),
 	creationDate      DATE,
 	preferredLanguage VARCHAR2(20),
 	tierID            VARCHAR2(50),
-};
+);
 
 /*-------------------------------*
  | MembershipTier Table Creation |
  *-------------------------------*/
-CREATE TABLE MembershipTier {
+CREATE TABLE MembershipTier (
     tierID            INTEGER PRIMARY KEY,
     tierName          VARCHAR2(50),
     maxMessagesPerDay Number(6),   -- Guessing 999,999 max messages is enough
     proModelStatus    Number(1),   -- True or false (1 or 0)
     monthlyFee        Number(10,2) -- Accounting for cents
-};
+);
 
 /*-------------------------------*
  | BillingProfile Table Creation |
  *-------------------------------*/
-CREATE TABLE BillingProfile {
+CREATE TABLE BillingProfile (
     billingID      INTEGER PRIMARY KEY,
     userID         INTEGER, -- FK to AppUser
     paymentMethod  VARCHAR(15),
     billingAddress VARCHAR(50)
-};
+);
 
 /*------------------------*
  | Invoice Table Creation |
  *------------------------*/
-CREATE TABLE Invoice {
+CREATE TABLE Invoice (
     invoiceID     INTEGER PRIMARY KEY,
     userID        INTEGER,      -- FK to AppUser
     amount        NUMBER(10,2), -- Accounting for cents
     invoiceDate   DATE,
     paymentStatus NUMBER(1)     -- True or false (1 or 0)
-};
+);
 
 /*------------------------------*
  | SupportTicket Table Creation |
  *------------------------------*/
-CREATE TABLE SupportTicket {
+CREATE TABLE SupportTicket (
     ticketID       INTEGER PRIMARY KEY,
     userID         INTEGER, -- FK to AppUser
     agentID        INTEGER, -- FK to SuppAgent
@@ -54,41 +64,41 @@ CREATE TABLE SupportTicket {
     dateOpened     DATE,
     resolutionDays NUMBER(5),
     outcome        VARCHAR2(1000)
-};
+);
 
 /*-----------------------------*
  | SupportAgent Table Creation |
  *-----------------------------*/
-CREATE TABLE SupportAgent {
+CREATE TABLE SupportAgent (
     agentID    INTEGER PRIMARY KEY,
     agentName  VARCHAR2(50),
     agentEmail VARCHAR2(50)
-};
+);
 
 /*--------------------------*
  | Workspace Table Creation |
  *--------------------------*/
-CREATE TABLE Workspace {
+CREATE TABLE Workspace (
     workspaceID   INTEGER PRIMARY KEY,
     creatorID     INTEGER, -- FK to AppUser
     workSpaceName VARCHAR2(50),
     privateStatus Number(1),
     creationDate  DATE
-};
+);
 
 /*------------------------------------*
  | WorkspaceMembership Table Creation |
  *------------------------------------*/
-CREATE TABLE WorkspaceMembership {
+CREATE TABLE WorkspaceMembership (
     userID      INTEGER,
     workspaceID INTEGER, -- FK to Workspace
     joinDate    DATE
-};
+);
 
 /*-----------------------------*
  | Conversation Table Creation |
  *-----------------------------*/
-CREATE TABLE Conversation {
+CREATE TABLE Conversation (
     conversationID INTEGER PRIMARY KEY,
     userID         INTEGER, -- FK to AppUser
     workspaceID    INTEGER, -- FK to Workspace
@@ -96,54 +106,54 @@ CREATE TABLE Conversation {
     title          VARCHAR2(50),
     creationDate   DATE,
     activeStatus   NUMBER(1)
-};
+);
 
 /*------------------------*
  | Message Table Creation |
  *------------------------*/
-CREATE TABLE Message {
+CREATE TABLE Message (
     messageID      INTEGER PRIMARY KEY,
     conversationID INTEGER,     -- FK to Conversation
     messageRole    VARCHAR2(20),
     content        VARCHAR2(1000),
     timeSent       TIMESTAMP
-};
+);
 
 /*-------------------------*
  | Feedback Table Creation |
  *-------------------------*/
-CREATE TABLE Feedback {
+CREATE TABLE Feedback (
     feedbackID    INTEGER PRIMARY KEY,
     messageID     INTEGER,   -- FK to Message
     rating        Number(3), -- Rating between 0 and 100
     feedbackText  VARCHAR2(1000),
     timeSubmitted TIMESTAMP
-};
+);
 
 /*-------------------------*
  | Bookmark Table Creation |
  *-------------------------*/
-CREATE TABLE Bookmark {
+CREATE TABLE Bookmark (
     userID         INTEGER PRIMARY KEY,
     messageID      INTEGER,      -- FK to Message
     timeBookmarked TIMESTAMP
-};
+);
 
 /*------------------------*
  | Persona Table Creation |
  *------------------------*/
-CREATE TABLE Persona {
+CREATE TABLE Persona (
     personalID   INTEGER PRIMARY KEY,
     personaName  VARCHAR2(50),
     instructions VARCHCAR2(500),
     creatorID    INTEGER, -- FK to UserApp
     creationDate DATE
-};
+);
 
 /*-------------------------------*
  | PromptTemplate Table Creation |
  *-------------------------------*/
-CREATE TABLE PromptTemplate {
+CREATE TABLE PromptTemplate (
     templateID    INTEGER PRIMARY KEY,
     creatorID     INTEGER,      -- FK to AppUser
     workspaceID   INTEGER,      -- FK to Workspace
@@ -152,4 +162,4 @@ CREATE TABLE PromptTemplate {
     category      VARCHAR2(50),
     privateStatus NUMBER(1),
     creationDate  DATE
-};
+);
